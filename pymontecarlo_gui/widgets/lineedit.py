76302f978de9cb37b17f86e92d77c8cee67af6c7
Philippe Pinard <philippe.pinard@gmail.com>
2017-04-01 12:10:41 +0100

    Add multi float line edit widget.

--- pymontecarlo_gui/widgets/lineedit.py
+++ pymontecarlo_gui/widgets/lineedit.py
@@ -6,13 +6,59 @@ import math
 import locale
 
 # Third party modules.
-from qtpy import QtWidgets, QtGui
+from qtpy import QtWidgets, QtGui, QtCore
 
 import numpy as np
 
 # Local modules.
 
 # Globals and constants variables.
+
+class DoubleValidatorAdapterMixin:
+
+    def _get_get_double_validator(self): #pragma: no cover
+        raise NotImplementedError
+
+    def bottom(self):
+        return self._get_double_validator().bottom()
+
+    def setBottom(self, bottom):
+        self._get_double_validator().setBottom(bottom)
+
+    def decimals(self):
+        return self._get_double_validator().decimals()
+
+    def setDecimals(self, decimals):
+        self._get_double_validator().setDecimals(decimals)
+
+    def range(self):
+        return self._get_double_validator().range()
+
+    def setRange(self, top, bottom, decimals=0):
+        self._get_double_validator().setRange(top, bottom, decimals)
+
+    def top(self):
+        return self._get_double_validator().top()
+
+    def setTop(self, top):
+        self._get_double_validator().setTop(top)
+
+class LineEditAdapterMixin:
+
+    def _get_lineedit(self): #pragma: no cover
+        raise NotImplementedError
+
+    def keyPressEvent(self, event):
+        self._get_lineedit().keyPressEvent(event)
+
+    def keyReleaseEvent(self, event):
+        self._get_lineedit().keyReleaseEvent(event)
+
+    def clear(self):
+        self._get_lineedit().clear()
+
+    def hasAcceptableInput(self):
+        return self._get_lineedit().hasAcceptableInput()
 
 class ColoredLineEdit(QtWidgets.QLineEdit):
 
@@ -21,9 +67,6 @@ class ColoredLineEdit(QtWidgets.QLineEdit):
 
         # Signals
         self.textChanged.connect(self._on_text_changed)
-
-        # Defaults
-        self._on_text_changed(self.text())
 
     def _on_text_changed(self, text):
         if self.hasAcceptableInput() or \
@@ -36,98 +79,148 @@ class ColoredLineEdit(QtWidgets.QLineEdit):
         super().setEnabled(enabled)
         self._on_text_changed(self.text())
 
-class ColoredFloatLineEdit(QtWidgets.QWidget):
+    def setValidator(self, validator):
+        super().setValidator(validator)
+        self._on_text_changed(self.text())
+
+class ColoredFloatLineEdit(QtWidgets.QWidget,
+                           LineEditAdapterMixin,
+                           DoubleValidatorAdapterMixin):
 
     def __init__(self, parent=None):
         super().__init__(parent)
 
         # Widgets
-        self.wdg_lineedit = ColoredLineEdit()
-        self.wdg_lineedit.setValidator(QtGui.QDoubleValidator())
+        self.lineedit = ColoredLineEdit()
+        self.lineedit.setValidator(QtGui.QDoubleValidator())
 
         # Layouts
         layout = QtWidgets.QHBoxLayout()
         layout.setContentsMargins(0, 0, 0, 0)
-        layout.addWidget(self.wdg_lineedit)
+        layout.addWidget(self.lineedit)
         self.setLayout(layout)
 
-        # Signals
-        self.wdg_lineedit.textChanged.connect(self._on_text_changed)
+    def _get_double_validator(self):
+        return self.lineedit.validator()
 
-        # Defaults
-        self._on_text_changed(self.wdg_lineedit.text())
-
-    def _on_text_changed(self, text):
-        if self.wdg_lineedit.hasAcceptableInput() or \
-                not self.isEnabled():
-            self.setStyleSheet("background: none")
-        else:
-            self.setStyleSheet("background: pink")
-
-    def keyPressEvent(self, event):
-        super().keyPressEvent(event)
-        self.wdg_lineedit.keyPressEvent(event)
-
-    def keyReleaseEvent(self, event):
-        super().keyReleaseEvent(event)
-        self.wdg_lineedit.keyReleaseEvent(event)
-
-    def clear(self):
-        self.wdg_lineedit.clear()
+    def _get_lineedit(self):
+        return self.lineedit
 
     def value(self):
         try:
-            return locale.atof(self.wdg_lineedit.text())
+            return locale.atof(self.lineedit.text())
         except ValueError:
             return float('nan')
 
     def setValue(self, value):
         fmt = '%.{}f'.format(self.decimals())
-        self.wdg_lineedit.setText(locale.format(fmt, value))
+        text = locale.format(fmt, value)
+        self.lineedit.setText(text)
 
-    def range(self):
-        return self.wdg_lineedit.validator().range()
+MULTIFLOAT_SEPARATOR = ';'
+MULTIFLOAT_PATTERN = r'(?P<start>inf|[\de\.+\-]*)(?:\:(?P<stop>[\de\.+\-]*))?(?:\:(?P<step>[\de\.+\-]*))?'
 
-    def setRange(self, top, bottom):
-        self.wdg_lineedit.validator().setRange(top, bottom)
-
-    def decimals(self):
-        return self.wdg_lineedit.validator().decimals()
-
-    def setDecimals(self, decimals):
-        self.wdg_lineedit.validator().setDecimals(decimals)
-
-def parse_text(text):
-    VALUE_SEPARATOR = ';'
-    PATTERN = r'(?P<start>inf|[\de\.+\-]*)(?:\:(?P<stop>[\de\.+\-]*))?(?:\:(?P<step>[\de\.+\-]*))?'
-
+def parse_multifloat_text(text):
     values = []
 
-    for part in text.split(VALUE_SEPARATOR):
+    for part in text.split(MULTIFLOAT_SEPARATOR):
         part = part.strip()
         if not part:
             continue
 
-        match = re.match(PATTERN, part)
+        match = re.match(MULTIFLOAT_PATTERN, part)
         if not match:
             raise ValueError('Invalid part: %s' % part)
 
-        start = float(match.group('start'))
-        stop = float(match.group('stop') or start + 1)
-        step = float(match.group('step') or 1)
+        start = locale.atof(match.group('start'))
+
+        stop = match.group('stop')
+        stop = locale.atof(stop) if stop is not None else start + 1
+
+        step = match.group('step')
+        step = locale.atof(step) if step is not None else 1
 
         if math.isinf(start):
             values.append(start)
         else:
             values.extend(np.arange(start, stop, step))
 
-    return np.array(values)
+    return sorted(set(values))
 
-def run():
+class MultiFloatValidator(QtGui.QValidator, DoubleValidatorAdapterMixin):
+
+    def __init__(self):
+        super().__init__()
+
+        expr = QtCore.QRegExp(r'^[\de\-.,+:;]+$')
+        self.validator_text = QtGui.QRegExpValidator(expr)
+        self.validator_value = QtGui.QDoubleValidator()
+
+    def validate(self, input, pos):
+        if not input:
+            return QtGui.QValidator.Intermediate, input, pos
+
+        state, input, pos = self.validator_text.validate(input, pos)
+        if state == QtGui.QValidator.Invalid:
+            return state, input, pos
+
+        try:
+            values = parse_multifloat_text(input)
+        except:
+            return QtGui.QValidator.Intermediate, input, pos
+
+        for value in values:
+            state, _, _ = self.validator_value.validate(str(value), pos)
+            if state != QtGui.QValidator.Acceptable:
+                return state, input, pos
+
+        return QtGui.QValidator.Acceptable, input, pos
+
+    def _get_double_validator(self):
+        return self.validator_value
+
+class ColoredMultiFloatLineEdit(QtWidgets.QWidget,
+                                LineEditAdapterMixin,
+                                DoubleValidatorAdapterMixin):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        # Widgets
+        self.lineedit = ColoredLineEdit()
+        self.lineedit.setValidator(MultiFloatValidator())
+
+        # Layouts
+        layout = QtWidgets.QHBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.addWidget(self.lineedit)
+        self.setLayout(layout)
+
+    def _get_double_validator(self):
+        return self.lineedit.validator()
+
+    def _get_lineedit(self):
+        return self.lineedit
+
+    def values(self):
+        try:
+            return parse_multifloat_text(self.lineedit.text())
+        except:
+            return ()
+
+    def setValues(self, values):
+        fmt = '%.{}f'.format(self.decimals())
+        text = MULTIFLOAT_SEPARATOR.join(locale.format(fmt, v) for v in values)
+        self.lineedit.setText(text)
+
+def run(): #pragma: no cover
     import sys
     app = QtWidgets.QApplication(sys.argv)
 
-    widget = ColoredFloatLineEdit()
+
+    widget = ColoredMultiFloatLineEdit()
+    widget.setRange(1.0, 5.0, 2)
+    widget.setValues([3.0, 4.12345])
 
     mainwindow = QtWidgets.QMainWindow()
     mainwindow.setCentralWidget(widget)
@@ -135,5 +228,5 @@ def run():
 
     app.exec_()
 
-if __name__ == '__main__':
+if __name__ == '__main__': #pragma: no cover
     run()
--- pymontecarlo_gui/widgets/test_lineedit.py
+++ pymontecarlo_gui/widgets/test_lineedit.py
@@ -6,11 +6,12 @@ import unittest
 import logging
 
 # Third party modules.
-from qtpy import QtTest, QtGui, QtCore
+from qtpy import QtTest, QtGui
 
 # Local modules.
 from pymontecarlo_gui.testcase import TestCase
-from pymontecarlo_gui.widgets.lineedit import ColoredLineEdit
+from pymontecarlo_gui.widgets.lineedit import \
+    ColoredLineEdit, ColoredFloatLineEdit, ColoredMultiFloatLineEdit
 
 # Globals and constants variables.
 
@@ -22,22 +23,125 @@ class TestColoredLineEdit(TestCase):
         self.wdg = ColoredLineEdit()
         self.wdg.setValidator(QtGui.QIntValidator(10, 50))
 
+    def testinitial_state(self):
+        self.assertFalse(self.wdg.hasAcceptableInput())
+        self.assertEqual('background: pink', self.wdg.styleSheet())
+
+        wdg = ColoredLineEdit()
+        self.assertTrue(wdg.hasAcceptableInput())
+        self.assertEqual('', wdg.styleSheet())
+
     def testsetText(self):
         self.wdg.setText('33')
+        self.assertTrue(self.wdg.hasAcceptableInput())
         self.assertEqual('background: none', self.wdg.styleSheet())
 
         self.wdg.setText('0')
+        self.assertFalse(self.wdg.hasAcceptableInput())
         self.assertEqual('background: pink', self.wdg.styleSheet())
 
     def testenter_text(self):
-        QtTest.QTest.mouseClick(self.wdg, QtCore.Qt.LeftButton)
-        QtTest.QTest.keyPress(self.wdg, QtCore.Qt.Key_3)
+        QtTest.QTest.keyClicks(self.wdg, '3')
         self.assertEqual('3', self.wdg.text())
+        self.assertFalse(self.wdg.hasAcceptableInput())
         self.assertEqual('background: pink', self.wdg.styleSheet())
 
-        QtTest.QTest.keyPress(self.wdg, QtCore.Qt.Key_3)
+        QtTest.QTest.keyClicks(self.wdg, '3')
         self.assertEqual('33', self.wdg.text())
+        self.assertTrue(self.wdg.hasAcceptableInput())
         self.assertEqual('background: none', self.wdg.styleSheet())
+
+class TestColoredFloatLineEdit(TestCase):
+
+    def setUp(self):
+        super().setUp()
+
+        self.wdg = ColoredFloatLineEdit()
+        self.wdg.setRange(10.0, 50.0, 2)
+
+    def testsetValue(self):
+        self.wdg.setValue(33)
+        self.assertTrue(self.wdg.hasAcceptableInput())
+        self.assertEqual('background: none', self.wdg.lineedit.styleSheet())
+
+        self.wdg.setValue(0)
+        self.assertFalse(self.wdg.hasAcceptableInput())
+        self.assertEqual('background: pink', self.wdg.lineedit.styleSheet())
+
+    def testenter_text(self):
+        QtTest.QTest.keyClicks(self.wdg, '3')
+        self.assertAlmostEqual(3.0, self.wdg.value(), 4)
+        self.assertFalse(self.wdg.hasAcceptableInput())
+        self.assertEqual('background: pink', self.wdg.lineedit.styleSheet())
+
+        QtTest.QTest.keyClicks(self.wdg, '3')
+        self.assertAlmostEqual(33.0, self.wdg.value(), 4)
+        self.assertTrue(self.wdg.hasAcceptableInput())
+        self.assertEqual('background: none', self.wdg.lineedit.styleSheet())
+
+class TestColoredMultiFloatLineEdit(TestCase):
+
+    def setUp(self):
+        super().setUp()
+
+        self.wdg = ColoredMultiFloatLineEdit()
+        self.wdg.setRange(10.0, 50.0, 2)
+
+    def testsetValues(self):
+        self.wdg.setValues([12.0, 45.0])
+        self.assertTrue(self.wdg.hasAcceptableInput())
+        self.assertEqual('background: none', self.wdg.lineedit.styleSheet())
+
+        self.wdg.setValues([0.0, 12.0, 45.0])
+        self.assertFalse(self.wdg.hasAcceptableInput())
+        self.assertEqual('background: pink', self.wdg.lineedit.styleSheet())
+
+    def testsetValues_decimals(self):
+        self.wdg.setValues([12.0, 45.123456])
+        self.assertTrue(self.wdg.hasAcceptableInput())
+        self.assertEqual('background: none', self.wdg.lineedit.styleSheet())
+
+        values = self.wdg.values()
+        self.assertEqual(2, len(values))
+        self.assertAlmostEqual(12.0, values[0], 4)
+        self.assertAlmostEqual(45.12, values[1], 4)
+
+    def testenter_text(self):
+        QtTest.QTest.keyClicks(self.wdg, '3')
+        self.assertFalse(self.wdg.hasAcceptableInput())
+        self.assertEqual('background: pink', self.wdg.lineedit.styleSheet())
+
+        values = self.wdg.values()
+        self.assertEqual(1, len(values))
+        self.assertAlmostEqual(3.0, values[0], 4)
+
+        QtTest.QTest.keyClicks(self.wdg, '3')
+        self.assertTrue(self.wdg.hasAcceptableInput())
+        self.assertEqual('background: none', self.wdg.lineedit.styleSheet())
+
+        values = self.wdg.values()
+        self.assertEqual(1, len(values))
+        self.assertAlmostEqual(33.0, values[0], 4)
+
+        QtTest.QTest.keyClicks(self.wdg, ';12.0')
+        self.assertTrue(self.wdg.hasAcceptableInput())
+        self.assertEqual('background: none', self.wdg.lineedit.styleSheet())
+
+        values = self.wdg.values()
+        self.assertEqual(2, len(values))
+        self.assertAlmostEqual(12.0, values[0], 4)
+        self.assertAlmostEqual(33.0, values[1], 4)
+
+        QtTest.QTest.keyClicks(self.wdg, ';20:40:10')
+        self.assertTrue(self.wdg.hasAcceptableInput())
+        self.assertEqual('background: none', self.wdg.lineedit.styleSheet())
+
+        values = self.wdg.values()
+        self.assertEqual(4, len(values))
+        self.assertAlmostEqual(12.0, values[0], 4)
+        self.assertAlmostEqual(20.0, values[1], 4)
+        self.assertAlmostEqual(30.0, values[2], 4)
+        self.assertAlmostEqual(33.0, values[3], 4)
 
 if __name__ == '__main__': #pragma: no cover
     logging.getLogger().setLevel(logging.DEBUG)
