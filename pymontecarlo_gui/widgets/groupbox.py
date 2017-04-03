6a3f8082f0c4adac745c7572486d126db58a9db5
Philippe Pinard <philippe.pinard@gmail.com>
2017-04-03 22:25:58 +0100

    Allow layout in group box

--- pymontecarlo_gui/widgets/groupbox.py
+++ pymontecarlo_gui/widgets/groupbox.py
@@ -9,13 +9,16 @@ from qtpy import QtWidgets
 
 # Globals and constants variables.
 
-def create_group_box(title, *widgets, direction=None):
+def create_group_box(title, *widgets_or_layouts, direction=None):
     if direction is None:
         direction = QtWidgets.QBoxLayout.TopToBottom
 
     layout = QtWidgets.QBoxLayout(direction)
-    for widget in widgets:
-        layout.addWidget(widget)
+    for item in widgets_or_layouts:
+        if isinstance(item, QtWidgets.QWidget):
+            layout.addWidget(item)
+        elif isinstance(item, QtWidgets.QLayout):
+            layout.addLayout(item)
 
     box = QtWidgets.QGroupBox(title)
     box.setLayout(layout)
